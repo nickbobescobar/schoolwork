3189997f63cec6ca07148add8ccd50f109001b6e
nickbobescobar <nre3@rice.edu>
2021-09-18 15:40:41 -0500

    started hw1_part1

---
+++ HW1/hw1_part1.sh
@@ -0,0 +1,13 @@
+# Nicolas Escobar
+# CAAM 519, Homework 1
+# Part 1
+
+# Make directory in whatever folder the script is run in and cd into it
+mkdir ./hw_part1
+cd ./hw_part1
+
+# Download and unpack archive
+wget https://msarrafj.github.io/CAAM519-FA21/Homeworks/HW1/hw1_materials.tar.gz
+tar -xf hw1_materials.tar.gz
+
+
